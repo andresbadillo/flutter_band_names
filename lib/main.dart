e54955c3bc382c712ecfbf65c3ffeb19efb52405
Andres Badillo <r.andres.badillo@gmail.com>
2022-11-28 13:32:45 -0500

    Se creo el modelo: band

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'package:band_names/pages/home.dart';
 
 void main() => runApp(const MyApp());
 
@@ -7,16 +10,17 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ));
+
     return MaterialApp(
-      title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Material App Bar'),
-        ),
-        body: const Center(
-          child: Text('Hello World'),
-        ),
-      ),
+      debugShowCheckedModeBanner: false,
+      title: 'Band Names App',
+      initialRoute: 'home',
+      routes: {
+        'home': (context) => HomePage(),
+      },
     );
   }
 }
